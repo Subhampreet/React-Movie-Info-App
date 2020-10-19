dcb598ffeea05b7264323ff112dafb68b43458d0
subhampreet <subhampreet18@gmail.com>
2020-10-19 22:02:18 +0530

    Search Feature Added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 
 import Movie from "./components/Movie";
 
-import Header from "./components/Header";
-
 const FEATURED_API =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5d668a4e55e28292714d6fdd2fd67935&page=1";
 
@@ -23,16 +21,49 @@ function App() {
       });
   }, []);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className="App">
-      <Header />
+      <div className="header">
+        <div className="header-left">
+          <img src="/logo.png" className="logo" />
+          <h1>
+            React <span>Movie</span>{" "}
+          </h1>
+        </div>
+
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search a Movie..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </div>
       <div className="tiles">
-      <div className="movie-container">
-        {movies.length > 0 &&
-          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+        <div className="movie-container">
+          {movies.length > 0 &&
+            movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 }
